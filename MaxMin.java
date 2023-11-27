bfd25c3042ccbf35dc30d8389a1d875a0d5b63df
sunnyok03 <sunnyok03@gmail.com>
2023-11-28 00:05:28 +0530

    UC3 used String class and compareTo

--- MaxMin.java
+++ MaxMin.java
@@ -4,9 +4,9 @@ import java.util.Scanner;
 
 public class MaxMin {
     public static void main(String[] args) {
-        test(1.1f,2.2f,3.3f);
-        test(2.2f,3.3f,1.1f);
-        test(3.3f,1.1f,2.2f);
+        test("Apple","Peach","Banana");
+        test("Peach","Banana","Apple");
+        test("Banana","Apple","Peach");
     }
 
     /*
@@ -14,8 +14,8 @@ public class MaxMin {
     @params:a,b,c (three float)
     @return: max(a,b,c)
      */
-    public static Float findMaxValue(float a,float b,float c){
-        Float res = a;
+    public static String findMaxValue(String a,String b,String c){
+        String res = a;
         if(res.compareTo(b) < 0){
             res = b;
         }
@@ -30,9 +30,9 @@ public class MaxMin {
     @params: a,b,c (three numbers)
     @return:
      */
-    public static void test(float a,float b,float c){
-        float maxVal = findMaxValue(a,b,c);
-        System.out.printf("Max among %f, %f, %f is %f",a,b,c,maxVal);
+    public static void test(String a,String b,String c){
+        String maxVal = findMaxValue(a,b,c);
+        System.out.printf("Max among %s, %s, %s is %s",a,b,c,maxVal);
         System.out.println();
     }
 }
